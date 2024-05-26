55de8648c947884252beeb8f113959d1937d6598
Tiago Corrêa <developer@tiagocorrea.me>
2024-05-26 19:38:31 -0300

    Update Sum Tails slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,4 +1,4 @@
-package arrays
+package main
 
 import (
 	"reflect"
@@ -6,11 +6,11 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
-		want := 6
+		want := 15
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
@@ -29,14 +29,12 @@ func TestSumAllTails(t *testing.T) {
 	t.Run("make the sums of some slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
-
 		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
 		checkSums(t, got, want)
 	})
 }
