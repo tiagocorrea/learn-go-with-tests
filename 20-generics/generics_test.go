0ee7863c054bc140bac4a953407cada884d6f195
Tiago Corrêa <developer@tiagocorrea.me>
2024-07-12 13:55:46 -0300

    Generics initial implementation, assertions without generics

---
+++ 20-generics/generics_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+
+	AssertEqual(t, 1, "1") // uncomment to see the error
+}
+
+func AssertEqual(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+func AssertNotEqual(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %+v", got)
+	}
+}
