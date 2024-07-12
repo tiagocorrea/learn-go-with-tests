18af318520ecf201bd30d48ad603f4dee5444fdb
Tiago Corrêa <developer@tiagocorrea.me>
2024-07-12 19:48:38 -0300

    arrays-slices-generics: find function

---
+++ 21-arrays-slices-generics/collection_fun_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 2)
+	})
+
+	type Person struct {
+		Name string
+	}
+
+	t.Run("find the best programmer", func(t *testing.T) {
+		people := []Person{
+			{Name: "Kent Beck"},
+			{Name: "Martin Fowler"},
+			{Name: "Tiago Corrêa"},
+		}
+
+		king, found := Find(people, func(p Person) bool {
+			return strings.Contains(p.Name, "Tiago")
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, king, Person{Name: "Tiago Corrêa"})
+	})
+}
