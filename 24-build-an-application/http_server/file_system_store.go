e3fa1576e63786a5a74d1c71f3800470d8e364c6
Tiago Corrêa <developer@tiagocorrea.me>
2024-08-03 17:31:30 -0300

    Build an application: persist scores

---
+++ 24-build-an-application/http_server/file_system_store.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type League []Player
+
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+	return nil
+}
+
+type FileSystemPlayerStore struct {
+	database io.ReadWriteSeeker
+	league   League
+}
+
+func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
+	database.Seek(0, io.SeekStart)
+	league, _ := NewLeague(database)
+	return &FileSystemPlayerStore{
+		database: database,
+		league:   league,
+	}
+}
+
+func (f *FileSystemPlayerStore) GetLeague() League {
+	return f.league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	player := f.league.Find(name)
+
+	if player != nil {
+		return player.Wins
+	}
+
+	return 0
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	player := f.league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	} else {
+		f.league = append(f.league, Player{name, 1})
+	}
+
+	f.database.Seek(0, io.SeekStart)
+	json.NewEncoder(f.database).Encode(f.league)
+}
--- 24-build-an-application/http_server/main.go
+++ 24-build-an-application/http_server/main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	server := NewPlayerServer(NewInMemoryPlayerStore())
-	log.Fatal(http.ListenAndServe(":8080", server))
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := NewFileSystemPlayerStore(db)
+	server := NewPlayerServer(store)
+
+	if err := http.ListenAndServe(":5001", server); err != nil {
+		log.Fatalf("could not listen on port 5001 %v", err)
+	}
 }
